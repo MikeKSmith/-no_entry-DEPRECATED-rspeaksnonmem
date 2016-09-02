1868d5f01455e156c3ac9a49b517ed9a0fcaf00d
Mike K Smith <Mike.K.Smith@Pfizer.com>
2016-09-02 14:06:09 +0100

    Improve functions to extract NM elements (Data, Parameters, Task) to return raw and parsed code.

--- R/extractNMObjects.R
+++ R/extractNMObjects.R
@@ -1,77 +1,118 @@
 ## ---getNMBlocks: identifies the various blocks of a NONMEM control stream
 getNMBlocks <- function(RNMImportObject) {
-    Raw <- RNMImportObject[[1]]
-    blocks <- grep("^\\$", Raw)
-    nextBlock <- c(blocks[-1], length(Raw))
-    ## Drop commented out lines blocks<-blocks[-grep('[;]',blocks)] Get first 'word' to
-    ## determine order
-    blocks2 <- sub(" +.*", "", Raw[blocks])
-    blocks3 <- sub("$", "", blocks2, fixed = T)
-    data.frame(Blocks = blocks2, Search = blocks3, firstRow = blocks, nextBlockRow = nextBlock)
+  Raw <- RNMImportObject$Raw
+  blocks <- grep("\\$", Raw)
+  nextBlock <- c(blocks[-1], length(Raw))
+  ## Drop commented out lines blocks<-blocks[-grep('[;]',blocks)] Get first 'word' to
+  ## determine order
+  blocks2 <- sub(" +.*", "", Raw[blocks])
+  blocks3 <- sub("$", "", blocks2, fixed = T)
+  data.frame(Blocks = blocks2, Search = blocks3, firstRow = blocks, nextBlockRow = nextBlock)
 }
 
 ## ---getNMDataObjects: Reads the $DATA and $INPUT records and parses them
-getNMDataObjects <- function(RNMImportObject) {
-    Raw <- RNMImportObject[[1]]
-    Parsed <- RNMImportObject[[4]][[1]]
-    
-    blockInfo <- getNMBlocks(RNMImportObject)
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "DATA", c("firstRow", "nextBlockRow")])
-    rawDataRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "INPUT", c("firstRow", "nextBlockRow")])
-    rawInputRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    RAW <- c(rawDataRows, rawInputRows)
-    if (length(grep("^\\;", RAW)) > 0) {
-        RAW <- RAW[-grep("^\\;", RAW)]
-    }
-    
-    list(RAW = RAW, HEADER = Parsed$Input, FILE = Parsed$Data)
+getNMDataObjects <- function(RNMImportObject, problemNo=1) {
+  Raw <- RNMImportObject$Raw
+  Parsed <- RNMImportObject$problemContents[[problemNo]]
+  
+  blockInfo <- getNMBlocks(RNMImportObject)
+  ## What rows correspond to each parameter type  
+  rowsData <- data.frame( blockInfo[grep("DAT",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  rowsInput <- data.frame( blockInfo[grep("INP",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+
+  ## Extract DATA rows
+  rawDataRows <- NULL 
+  if ( length( grep( "DAT", as.character( blockInfo$Search ) ) ) > 0 ){
+    rawDataRows <- apply( rowsData, 1, function(x) Raw[ x[1] : (x[2] - 1) ] )
+  }
+  
+  ## Extract INPUT rows
+  rawInputRows <- NULL
+  if ( length( grep( "OME", as.character( blockInfo$Search ) ) ) > 0 ) {
+    rawInputRows <- apply( rowsInput, 1, function(x) Raw[ x[1] : (x[2] - 1) ] )
+  }
+
+  RAW <- unlist( c( rawInputRows, rawDataRows ) )
+  if (length(grep("^\\;", RAW)) > 0) {
+    RAW <- RAW[-grep("^\\;", RAW)]
+  }
+  
+  list(RAW = RAW, INPUT = Parsed$Input, DATA = Parsed$Data)
 }
 
 ## ---getNMParameterObjects: Reads the $THETA, $OMEGA, $SIGMA records and parses them
-getNMParameterObjects <- function(RNMImportObject) {
-    Raw <- RNMImportObject[[1]]
-    Parsed <- RNMImportObject[[4]][[1]]
-    
-    blockInfo <- getNMBlocks(RNMImportObject)
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "THETA", c("firstRow", "nextBlockRow")])
-    rawThetaRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "OMEGA", c("firstRow", "nextBlockRow")])
-    rawOmegaRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "SIGMA", c("firstRow", "nextBlockRow")])
-    rawSigmaRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    RAW <- c(rawThetaRows, rawOmegaRows, rawSigmaRows)
-    if (length(grep("^\\;", RAW)) > 0) {
-        RAW <- RAW[-grep("^\\;", RAW)]
-    }
-    
-    list(RAW = RAW, STRUCTURAL = Parsed$Theta, VARIABILITY = list(IIV = Parsed$Omega, RUV = Parsed$Sigma))
+getNMParameterObjects <- function(RNMImportObject, problemNo=1) {
+  Raw <- RNMImportObject$Raw
+  Parsed <- RNMImportObject$problemContents[[problemNo]]
+  
+  blockInfo <- getNMBlocks(RNMImportObject)
+  
+  ## What rows correspond to each parameter type  
+  rowsTheta <- data.frame( blockInfo[grep("THE",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  rowsOmega <- data.frame( blockInfo[grep("OME",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  rowsSigma <- data.frame( blockInfo[grep("SIG",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  
+  ## Extract THETA rows (if there are any)
+  rawThetaRows <- NULL 
+  if ( length( grep( "THE", as.character( blockInfo$Search ) ) ) > 0 ){
+    rawThetaRows <- apply( rowsTheta, 1, function(x) Raw[ x[1] : (x[2] - 1) ] )
+  }
+  
+  ## Extract OMEGA rows (if there are any)
+  rawOmegaRows <- NULL
+  if ( length( grep( "OME", as.character( blockInfo$Search ) ) ) > 0 ) {
+    rawOmegaRows <- apply( rowsOmega, 1, function(x) Raw[ x[1] : (x[2] - 1) ] )
+  }
+  
+  ## Extract SIGMA rows (if there are any)
+  rawSigmaRows <- NULL
+  if ( length( grep( "SIG", as.character( blockInfo$Search ) ) ) > 0 ){
+    rawSigmaRows <- apply( rowsSigma, 1, function(x) Raw[ x[1] : (x[2] - 1) ] )
+  }
+  
+  RAW <- unlist( c( rawThetaRows, rawOmegaRows, rawSigmaRows ) )
+  if (length(grep("^\\;", RAW)) > 0) {
+    RAW <- RAW[-grep("^\\;", RAW)]
+  }
+  
+  list(RAW = RAW, THETA = Parsed$Theta, OMEGA = Parsed$Omega, SIGMA = Parsed$Sigma)
 }
 
 ## ---getNMTaskProperties: Reads the $EST, $COV, $TAB records and parses them
-getNMTaskPropertiesObjects <- function(RNMImportObject) {
-    Raw <- RNMImportObject[[1]]
-    Parsed <- RNMImportObject[[4]][[1]]
-    
-    blockInfo <- getNMBlocks(RNMImportObject)
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "EST", c("firstRow", "nextBlockRow")])
-    rawEstRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "COV", c("firstRow", "nextBlockRow")])
-    rawCovRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    rows <- unlist(blockInfo[as.character(blockInfo$Search) == "TAB", c("firstRow", "nextBlockRow")])
-    rawTableRows <- Raw[rows[1]:(rows[2] - 1)]
-    
-    RAW <- c(rawEstRows, rawCovRows)
-    if (length(grep("^\\;", RAW)) > 0) {
-        RAW <- RAW[-grep("^\\;", RAW)]
-    }
-    
-    list(RAW = RAW, TARGET_CODE = list(Parsed$Estimates, Parsed$Cov, Parsed$Tables))
+getNMTaskPropertiesObjects <- function(RNMImportObject, problemNo=1) {
+  Raw <- RNMImportObject$Raw
+  Parsed <- RNMImportObject$problemContents[[problemNo]]
+  
+  blockInfo <- getNMBlocks(RNMImportObject)
+  
+  ## What rows correspond to each parameter type  
+  rowsEST <- data.frame( blockInfo[grep("EST",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  rowsTAB <- data.frame( blockInfo[grep("TAB",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  rowsCOV <- data.frame( blockInfo[grep("COV",as.character(blockInfo$Search)), c("firstRow", "nextBlockRow")] , row.names=NULL)
+  
+  ## Extract EST rows (if there are any)
+  rawEstRows <- NULL 
+  if ( length( grep( "EST", as.character( blockInfo$Search ) ) ) > 0 ){
+    rawEstRows <- apply( rowsEST, 1, function(x) Raw[ x[1] : if(x[2]>x[1])(x[2] - 1) else (x[2]) ] )
+  }
+  
+  ## Extract TAB rows (if there are any)
+  rawTabRows <- NULL
+  if ( length( grep( "TAB", as.character( blockInfo$Search ) ) ) > 0 ) {
+    rawTabRows <- apply( rowsTAB, 1, function(x) Raw[ x[1] : if(x[2]>x[1])(x[2] - 1) else (x[2]) ] )
+  }
+  
+  ## Extract COV rows (if there are any)
+  rawCovRows <- NULL
+  if ( length( grep( "COV", as.character( blockInfo$Search ) ) ) > 0 ){
+    rawCovRows <- apply( rowsCOV, 1, function(x) Raw[ x[1] : if(x[2]>x[1])(x[2] - 1) else (x[2]) ] )
+  }
+  
+  RAW <- unlist( c( rawEstRows, rawTabRows, rawCovRows ) )
+  
+  if (length(grep("^\\;", RAW)) > 0) {
+    RAW <- RAW[-grep("^\\;", RAW)]
+  }
+  
+  list(RAW = RAW, ESTIMATES = Parsed$Estimates, COV = Parsed$Cov, TABLES = Parsed$Tables)
 } 
